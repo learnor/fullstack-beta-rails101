6a1aad598a72a0f112b638ec48e9cee506871d21
Learnor <learnor@gmail.com>
2016-11-20 21:18:23 -0600

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  namespace :account do
+    resources :groups
+  end
   devise_for :users
   resources :groups do
     member do
       post :join
       post :quit
     end
-    
+
     resources :posts
   end
   root 'groups#index'
